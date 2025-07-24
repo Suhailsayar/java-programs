310f3a81ee11172a5d9522b06e9b4fedda6099fd
Suhailsayar <suhailsahil508@gmail.com>
2025-07-24 21:45:07 +0530

    initial commit

---
+++ myfirstprogram.java
@@ -0,0 +1,6 @@
+public class myfirstprogram {
+    public static void main(String[]) {
+        System.out.println("Hello, World!");
+    }
+}
+// This is a simple Java program that prints "Hello, World!" to the console.
