48244e5fed0e8796c610b8ad195240538dd85b15
Suhailsayar <suhailsahil508@gmail.com>
2025-08-05 17:19:07 +0530

    concatenation of two strings

---
+++ concatenate.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Concatenate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String firstString = scanner.nextLine();
+        System.out.print("Enter the second string: ");
+        String secondString = scanner.nextLine();
+        String concatenatedString = firstString + secondString;
+        System.out.println("Concatenated String: " + concatenatedString);
+        scanner.close();
+    }
+}
