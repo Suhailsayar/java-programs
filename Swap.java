3752f4216a182a13ad5868d850a309d33b1a2171
Suhailsayar <suhailsahil508@gmail.com>
2025-08-07 22:33:18 +0530

    initial commit

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+        // Swapping values
+        a = a ^b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
