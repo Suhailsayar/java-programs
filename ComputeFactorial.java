2b037a8b3d94719a43ed936dcb212846ca2fd069
Suhailsayar <suhailsahil508@gmail.com>
2025-08-08 13:46:24 +0530

    initail commit

---
+++ ComputeFactorial.java
@@ -0,0 +1,17 @@
+
+ public class ComputeFactorial{
+ 
+    public static int factorial(int n){
+      if(n==0||n==1){
+        return 1;
+      }
+      else{
+        return(n*factorial(n-1));
+      }
+    }
+    public static void main(String s[]){
+      int number=5;
+      int result = factorial(number);
+      System.out.println("factorial="+result);
+    }
+}
